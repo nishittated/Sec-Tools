1c735c23b0aa9044787599ddbba752999b4dadd1
Nishit Tated <nishittated@gmail.com>
2018-01-07 23:29:35 +0530

    Add files via upload

---
+++ base64.php
@@ -0,0 +1,311 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+
+	$Pword = trim(isset($_POST['word']) ? strtolower($_POST['word']) : null );
+	$md = base64_encode(strtolower($Pword));	
+	
+}
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+
+	$Pword = trim(isset($_POST['word']) ? strtolower($_POST['word']) : null );
+	$md1 = base64_decode($md);	
+	
+}
+?>
+
+<head>
+	<head>
+    <meta charset="UTF-8">
+    <!--IE Compatibility modes-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--Mobile first-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Base64 | Encoding-Decoding</title>
+    
+    <meta name="description" content="Free Admin Template Based On Twitter Bootstrap 3.x">
+    <meta name="author" content="">
+    
+    <meta name="msapplication-TileColor" content="#5bc0de" />
+    <meta name="msapplication-TileImage" content="assets/img/metis-tile.png" />
+    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/lib/bootstrap/css/bootstrap.css">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="assets/lib/font-awesome/css/font-awesome.css">
+    
+    <!-- Metis core stylesheet -->
+    <link rel="stylesheet" href="assets/css/main.css">
+    
+    <!-- metisMenu stylesheet -->
+    <link rel="stylesheet" href="assets/lib/metismenu/metisMenu.css">
+    
+    <!-- onoffcanvas stylesheet -->
+    <link rel="stylesheet" href="assets/lib/onoffcanvas/onoffcanvas.css">
+    
+    <!-- animate.css stylesheet -->
+    <link rel="stylesheet" href="assets/lib/animate.css/animate.css">
+
+
+        <link rel="stylesheet" href="/assets/lib/bootstrap3-wysihtml5-bower/dist/bootstrap3-wysihtml5.min.css">
+        <link rel="stylesheet" href="/assets/lib/pagedown-bootstrap/css/jquery.pagedown-bootstrap.css">
+		
+		
+		<script>
+        less = {
+            env: "development",
+            relativeUrls: false,
+            rootpath: "/assets/"
+        };
+    </script>
+    <link rel="stylesheet" href="assets/css/style-switcher.css">
+    <link rel="stylesheet/less" type="text/css" href="assets/less/theme.less">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.js"></script>
+
+	</head>
+
+        <body class="  ">
+            <div class="bg-dark dk" id="wrap">
+                <div id="top">
+                    <!-- .navbar -->
+                    <nav class="navbar navbar-inverse navbar-static-top">
+                        <div class="container-fluid">
+                    
+                    
+                            <!-- Brand and toggle get grouped for better mobile display -->
+                            <header class="navbar-header">
+                    
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a href="index.html" class="navbar-brand"><img src="assets/img/logo.png" alt=""></a>
+                    
+                            </header>
+                    
+                    
+                    
+                            <div class="topnav">
+                                <div class="btn-group">
+                                    <a data-placement="bottom" data-original-title="Fullscreen" data-toggle="tooltip"
+                                       class="btn btn-default btn-sm" id="toggleFullScreen">
+                                        <i class="glyphicon glyphicon-fullscreen"></i>
+                                    </a>
+                                </div>
+                                <div class="btn-group">
+                                    <a data-placement="bottom" data-original-title="E-mail" data-toggle="tooltip"
+                                       class="btn btn-default btn-sm">
+                                        <i class="fa fa-envelope"></i>
+                                        <span class="label label-warning">5</span>
+                                    </a>
+                                    <a data-placement="bottom" data-original-title="Messages" href="#" data-toggle="tooltip"
+                                       class="btn btn-default btn-sm">
+                                        <i class="fa fa-comments"></i>
+                                        <span class="label label-danger">4</span>
+                                    </a>
+                                    <a data-toggle="modal" data-original-title="Help" data-placement="bottom"
+                                       class="btn btn-default btn-sm"
+                                       href="#helpModal">
+                                        <i class="fa fa-question"></i>
+                                    </a>
+                                </div>
+                                <div class="btn-group">
+                                    <a href="login.html" data-toggle="tooltip" data-original-title="Logout" data-placement="bottom"
+                                       class="btn btn-metis-1 btn-sm">
+                                        <i class="fa fa-power-off"></i>
+                                    </a>
+                                </div>
+                                <div class="btn-group">
+                                    <a data-placement="bottom" data-original-title="Show / Hide Left" data-toggle="tooltip"
+                                       class="btn btn-primary btn-sm toggle-left" id="menu-toggle">
+                                        <i class="fa fa-bars"></i>
+                                    </a>
+                                    <a href="#right" data-toggle="onoffcanvas" class="btn btn-default btn-sm" aria-expanded="false">
+                                        <span class="fa fa-fw fa-comment"></span>
+                                    </a>
+                                </div>
+                    
+                            </div>
+                    
+                    
+                    
+                    
+                            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    
+                                <!-- .nav -->
+                                <ul class="nav navbar-nav">
+                                    <li><a href="dashboard.html">Dashboard</a></li>
+                                    <!-- <li><a href="table.html">Tables</a></li>  -->
+                                    									
+									<li class='dropdown '>
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                            Encryption-Decryption <b class="caret"></b>
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="md5.html">MD5</a></li>
+											   
+                                            <li><a href="sha.php">SHA1</a></li>
+                                        </ul>
+                                    </li>
+									
+									
+									
+									<li class='dropdown '>
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                            Binary Analysis <b class="caret"></b>
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="filescan.php">Filescan</a></li>
+											   
+                                            <li><a href="url.php">URL</a></li>
+											
+											<li><a href="virus.php">Virus</a></li> 
+                                        </ul>
+                                    </li>
+									
+									<li class='dropdown '>
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                            Encoding-Decoding <b class="caret"></b>
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="base64.php">Base64</a></li>
+											   
+                                            <li><a href="urlende.php">URLende</a></li>
+                                        </ul>
+                                    </li>
+									
+                                </ul>
+                                <!-- /.nav -->
+                            </div>
+                        </div>
+                        <!-- /.container-fluid -->
+                    </nav>
+                    <!-- /.navbar -->
+                        <header class="head">
+                                <div class="search-bar">
+                                    <form class="main-search" action="">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control" placeholder="Live Search ...">
+                                            <span class="input-group-btn">
+                                                <button class="btn btn-primary btn-sm text-muted" type="button">
+                                                    <i class="fa fa-search"></i>
+                                                </button>
+                                            </span>
+                                        </div>
+                                    </form>
+                                    <!-- /.main-search -->                                </div>
+                                <!-- /.search-bar -->
+                            <div class="main-bar">
+                                <h3>
+              <i class="fa fa-home"></i>&nbsp;
+            
+          </h3>
+                            </div>
+                            <!-- /.main-bar -->
+                        </header>
+                        <!-- /.head -->
+                </div>
+                <!-- /#top -->
+                    <div id="left">
+                        <div class="media user-media bg-dark dker">
+                            <div class="user-media-toggleHover">
+                                <span class="fa fa-user"></span>
+                            </div>
+                            <div class="user-wrapper bg-dark">
+                                <a class="user-link" href="">
+                                    <img class="media-object img-thumbnail user-img" alt="User Picture" src="assets/img/user.gif">
+                                    <span class="label label-danger user-label">16</span>
+                                </a>
+                        
+                                <div class="media-body">
+                                    <h5 class="media-heading">Archie</h5>
+                                    <ul class="list-unstyled user-info">
+                                        <li><a href="">Administrator</a></li>
+                                        <li>Last Access : <br>
+                                            <small><i class="fa fa-calendar"></i>&nbsp;14 April 16:32</small>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- #menu -->
+                        <ul id="menu" class="bg-blue dker">
+                                  <li class="nav-header">Menu</li>
+                                  <li class="nav-divider"></li>
+                                  <li class="">
+                                    <a href="dashboard.html">
+                                      <i class="fa fa-dashboard"></i><span class="link-title">&nbsp;Dashboard</span>
+                                    </a>
+                                  </li>
+                                  
+                                    </a>
+                                  </li>
+                                  <li class="nav-divider"></li>
+                                  <li>
+                                    <a href="login.html">
+                                      <i class="fa fa-sign-in"></i>
+                                      <span class="link-title">
+                            Login Page
+                            </span>
+                                    </a>
+                                  </li>
+                                 
+                        <!-- /#menu -->
+                    </div>
+                    <!-- /#left -->
+                <div id="content">
+                    <div class="outer">
+                        <div class="inner bg-light lter">
+						
+	<head>
+		<title>Base64 | Encoding-Decoding </title>
+	</head>
+<body><br /><br/>
+<div class="conrainer">
+	<form action="base64.php" method="POST" name="library">
+		<div class="">
+			<label for="word">String</label>
+			<input type="text" name="word" placeholder="Enter String.." />
+		</div>
+		<div class="">
+			<button type="submit" class="btn">Check</button>
+		</div>
+	</form>
+</div><br /> 
+	<?php 
+		if($_POST){
+
+			if($Pword != null || $Pword != ""){			
+			echo "Base64 Encoding  for string ". $Pword. " is   " . $md ;	
+			}
+		}
+	 ?>
+ <br /> <br />
+<div class="container">
+	
+	<form action="base64.php" method="GET" name="library">
+		<div class="">
+			<label for="md5">Base64 Decoding:</label>
+			<input type="text" name="sha" placeholder="Enter encoding string" />
+		</div>
+		<div class="">
+			<button type="submit">Check Decoding</button>
+		</div>
+	</form>
+</div>
+	<?php 
+		if($_POST){
+
+			if($Pword != null || $Pword != ""){			
+			echo "Base64 Decoding  for string ". $md. " is   " . $md1 ;	
+			}
+		}
+	 ?>
+
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,213 @@
+<?php
+
+$servername="localhost";
+$username="root";
+$password="";
+$dbname = "nits";
+/*
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+
+}
+*/
+
+if($_SERVER['REQUEST_METHOD'] === "POST"){
+try{
+$con = new PDO('mysql:host=localhost;dbname=nits',$username,$password);
+} catch (PDOException $e){
+	echo $e -> getMessage();
+	die();
+}
+
+
+					$msg = '';            
+            if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+				
+                $username = $_POST['username'];
+				$password = $_POST['password'];
+				
+				$result = $con -> prepare("SELECT * FROM login WHERE Username='" .$username . "' AND Password='".$password ."'");
+				$result ->execute();
+				$result = $result -> fetchAll(PDO::FETCH_ASSOC);
+				
+				
+				if($result ){
+					$_SESSION['valid'] = true;
+                  $_SESSION['timeout'] = time();
+                  $_SESSION['username'] = $result["Username"];
+					
+					Header('Location:dashboard.html');
+				} else {
+					
+					echo 'error';
+					die();
+				}
+			   
+			   
+			     
+            }
+			
+			if(isset($_POST['signup']) && !empty($_POST['susername']) &&!empty($_POST['spass'])){
+				$username = $_POST['susername'];
+				$password = $_POST['spass'];
+				$email = $_POST['semail'];
+				$result = $con->exec("INSERT INTO login VALUES('$username','$password','$email')");
+				//$result->execute();
+				//$result = $result ->fetch();
+			
+				
+			}
+			
+			if(isset($_POST['fsubmit']) && !empty($_POST['femail'])){
+				$email = $_POST['femail'];
+				
+				$sth = $con->query("SELECT * FROM login WHERE email ='".$email."'");
+				$sth = $sth ->fetchAll(PDO::FETCH_ASSOC);
+				
+				var_dump($sth);
+				die();
+				$fpassword = $sth['Password'];
+				
+				
+			}
+			
+}
+         ?>
+	
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!--IE Compatibility modes-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--Mobile first-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Login Page</title>
+    
+    <meta name="description" content="Free Admin Template Based On Twitter Bootstrap 3.x">
+    <meta name="author" content="">
+    
+    <meta name="msapplication-TileColor" content="#5bc0de" />
+    <meta name="msapplication-TileImage" content="assets/img/metis-tile.png" />
+    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/lib/bootstrap/css/bootstrap.css">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="assets/lib/font-awesome/css/font-awesome.css">
+    
+    <!-- Metis core stylesheet -->
+    <link rel="stylesheet" href="assets/css/main.css">
+    
+    <!-- metisMenu stylesheet -->
+    <link rel="stylesheet" href="assets/lib/metismenu/metisMenu.css">
+    
+    <!-- onoffcanvas stylesheet -->
+    <link rel="stylesheet" href="assets/lib/onoffcanvas/onoffcanvas.css">
+    
+    <!-- animate.css stylesheet -->
+    <link rel="stylesheet" href="assets/lib/animate.css/animate.css">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body class="login">
+
+      <div class="form-signin">
+    <div class="text-center">
+        <img src="assets/img/Logo.png" >
+    </div>
+    <hr>
+    <div class="tab-content">
+        <div id="login" class="tab-pane active">
+			
+            <form action="login.php" method="POST">
+                <p class="text-muted text-center">
+                    Enter your username and password
+                </p>
+				<p>
+					<?php if(isset($fpassword)){
+						echo $fpassword;
+					} ?>
+				</p>
+                <input type="text" placeholder="username" name="username" class="form-control top">
+                <input type="password" placeholder="password" name="password" class="form-control bottom">
+                <div class="checkbox">
+		  <label>
+		    <input type="checkbox"> Remember Me
+		  </label>
+		</div>
+                <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Sign in</button>
+            </form>
+        </div>
+        <div id="forgot" class="tab-pane">
+            <form action="login.php" method="POST">
+                <p class="text-muted text-center">Enter your valid e-mail</p>
+                <input type="email" name="femail" placeholder="mail@domain.com" class="form-control">
+                <br>
+                <button class="btn btn-lg btn-danger btn-block" name="fsubmit" type="submit">Recover Password</button>
+            </form>
+        </div>
+        <div id="signup" class="tab-pane">
+            <form action="login.php" name="signup" method="POST">
+                <input type="text" placeholder="username" name="susername" class="form-control top">
+                <input type="email" placeholder="mail@domain.com" name="semail" class="form-control middle">
+                <input type="password" placeholder="password" name="spass" class="form-control middle">
+                <input type="password" placeholder="re-password" name="srepass" class="form-control bottom">
+                <button class="btn btn-lg btn-success btn-block" type="submit" name="signup">Register</button>
+            </form>
+        </div>
+    </div>
+    <hr>
+    <div class="text-center">
+        <ul class="list-inline">
+            <li><a class="text-muted" href="#login" data-toggle="tab">Login</a></li>
+            <li><a class="text-muted" href="#forgot" data-toggle="tab">Forgot Password</a></li>
+            <li><a class="text-muted" href="#signup" data-toggle="tab">Signup</a></li>
+        </ul>
+    </div>
+  </div>
+
+
+    <!--jQuery -->
+    <script src="assets/lib/jquery/jquery.js"></script>
+
+    <!--Bootstrap -->
+    <script src="assets/lib/bootstrap/js/bootstrap.js"></script>
+
+
+    <script type="text/javascript">
+        (function($) {
+            $(document).ready(function() {
+                $('.list-inline li > a').click(function() {
+                    var activeForm = $(this).attr('href') + ' > form';
+                    //console.log(activeForm);
+                    $(activeForm).addClass('animated fadeIn');
+                    //set timer to 1 seconds, after that, unload the animate animation
+                    setTimeout(function() {
+                        $(activeForm).removeClass('animated fadeIn');
+                    }, 1000);
+                });
+            });
+        })(jQuery);
+    </script>
+	
+	
+</body>
+</html>
