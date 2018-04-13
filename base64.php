ec32544ed7a8d3325cf43ff07b6a8092dbf46650
Nishit Tated <nishittated@gmail.com>
2018-04-13 14:04:52 +0530

    Update base64.php

--- base64.php
+++ base64.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
 	$md = base64_encode(strtolower($Pword));	
 	
 }
-if ($_SERVER["REQUEST_METHOD"] === "POST"){
+if ($_SERVER["REQUEST_METHOD"] === "POST"){ 
 
 	$Pword = trim(isset($_POST['word']) ? strtolower($_POST['word']) : null );
 	$md1 = base64_decode($md);	
